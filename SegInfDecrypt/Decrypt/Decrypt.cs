2c6dd8f692242bb5586f1a97fdc36b5e8b1c7c0a
Blackeye <122568941+gabrielCarvalhoGit@users.noreply.github.com>
2023-04-05 23:48:52 -0300

    Publish Project - Encrypt Files

---
+++ SegInfDecrypt/Decrypt/Decrypt.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decrypt
+{
+    public class Decrypt
+    {
+        public CspParameters cspp;
+        public RSACryptoServiceProvider rsa;
+
+        public string DecrFolder { get; set; }
+        public string keyName;
+
+        // Metódo para descriptografar um arquivo
+        public string DecryptFile(string inFile)
+        {
+            Aes aes = Aes.Create();
+
+            byte[] LenK = new byte[4];
+            byte[] LenIV = new byte[4];
+
+            string outFile = DecrFolder + inFile.Substring(0, inFile.LastIndexOf("."));
+
+            try
+            {
+                using (FileStream inFs = new FileStream(inFile, FileMode.Open))
+                {
+                    inFs.Seek(0, SeekOrigin.Begin);
+                    inFs.Seek(0, SeekOrigin.Begin);
+                    inFs.Read(LenK, 0, 3);
+                    inFs.Seek(4, SeekOrigin.Begin);
+                    inFs.Read(LenIV, 0, 3);
+
+                    int lenK = BitConverter.ToInt32(LenK, 0);
+                    int lenIV = BitConverter.ToInt32(LenIV, 0);
+
+                    int startC = lenK + lenIV + 8;
+                    int lenC = (int)inFs.Length - startC;
+
+                    byte[] KeyEncrypted = new byte[lenK];
+                    byte[] IV = new byte[lenIV];
+
+                    inFs.Seek(8, SeekOrigin.Begin);
+                    inFs.Read(KeyEncrypted, 0, lenK);
+                    inFs.Seek(8 + lenK, SeekOrigin.Begin);
+                    inFs.Read(IV, 0, lenIV);
+
+                    if (!Directory.Exists(DecrFolder))
+                    {
+                        Directory.CreateDirectory(DecrFolder);
+                    }
+
+                    byte[] KeyDecrypted = rsa.Decrypt(KeyEncrypted, false);
+
+                    ICryptoTransform transform = aes.CreateDecryptor(KeyDecrypted, IV);
+
+                    using (FileStream outFs = new FileStream(outFile, FileMode.Create))
+                    {
+                        int count = 0;
+                        int offset = 0;
+
+                        int blockSizeBytes = aes.BlockSize / 8;
+                        byte[] data = new byte[blockSizeBytes];
+
+                        inFs.Seek(startC, SeekOrigin.Begin);
+                        using (CryptoStream outStreamDecrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                        {
+                            do
+                            {
+                                count = inFs.Read(data, 0, blockSizeBytes);
+                                offset += count;
+                                outStreamDecrypted.Write(data, 0, count);
+                            } while (count > 0);
+
+                            outStreamDecrypted.FlushFinalBlock();
+                            outStreamDecrypted.Close();
+                        }
+                        outFs.Close();
+                    }
+                    inFs.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return $"Arquivo descriptografado.\n Origem: {inFile}\n Destino: {outFile}";
+        }
+    }
+}
---
+++ SegInfEncrypt/Encrypt/Encrypt.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography.Cryptography
+{
+    public class Encrypt
+    {
+        public CspParameters cspp;
+        public RSACryptoServiceProvider rsa;
+
+        private string _encrFolder;
+
+        public string EncrFolder
+        {
+            get
+            {
+                return _encrFolder;
+            }
+            set
+            {
+                _encrFolder = value;
+            }
+        }
+
+        public string SrcFolder { get; set; }
+
+        public string keyName;
+
+        //Método para criar uma chave privada através de um valor definido
+        public string GetPrivateKey()
+        {
+            string result = "";
+
+            if (string.IsNullOrEmpty(keyName))
+            {
+                result = "Chave privada não definida";
+                return result;
+            }
+
+            cspp.KeyContainerName = keyName;
+            rsa = new RSACryptoServiceProvider(cspp);
+            rsa.PersistKeyInCsp = true;
+
+            if (rsa.PublicOnly)
+            {
+                result = "Key: " + cspp.KeyContainerName + " - Somente Publica";
+            }
+            else
+            {
+                result = "Key: " + cspp.KeyContainerName + " - Key Pair Completa";
+            }
+
+            return result;
+        }
+
+        //Método para criptografar o arquivo
+        public string EncryptFile(string inFile)
+        {
+            Aes aes = Aes.Create();
+            ICryptoTransform transform = aes.CreateEncryptor();
+
+            byte[] keyEncrypted = rsa.Encrypt(aes.Key, false);
+
+            byte[] LenK = new byte[4];
+            byte[] LenIV = new byte[4];
+
+            int lKey = keyEncrypted.Length;
+            LenK = BitConverter.GetBytes(lKey);
+            int lIV = aes.IV.Length;
+            LenIV = BitConverter.GetBytes(lIV);
+
+            int startFileName = inFile.LastIndexOf("\\") + 1;
+            string outFile = EncrFolder + inFile.Substring(startFileName) + ".enc";
+
+            try
+            {
+                using (FileStream outFs = new FileStream(outFile, FileMode.Create))
+                {
+                    outFs.Write(LenK, 0, 4);
+                    outFs.Write(LenIV, 0, 4);
+                    outFs.Write(keyEncrypted, 0, lKey);
+                    outFs.Write(aes.IV, 0, lIV);
+
+                    using (CryptoStream outStreamEncrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                    {
+                        int count = 0;
+                        int offset = 0;
+
+                        int blockSizeBytes = aes.BlockSize / 8;
+                        byte[] data = new byte[blockSizeBytes];
+                        int bytesRead = 0;
+
+                        using (FileStream inFs = new FileStream(inFile, FileMode.Open))
+                        {
+                            do
+                            {
+                                count = inFs.Read(data, 0, blockSizeBytes);
+                                offset += count;
+                                outStreamEncrypted.Write(data, 0, count);
+                                bytesRead += blockSizeBytes;
+                            } while (count > 0);
+                            inFs.Close();
+                        }
+                        outStreamEncrypted.FlushFinalBlock();
+                        outStreamEncrypted.Close();
+                    }
+                    outFs.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return $"Arquivo criptografado.\n Origem: {inFile}\n Destino: {outFile}";
+        }
+    }
+}
